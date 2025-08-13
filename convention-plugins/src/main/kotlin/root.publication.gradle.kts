052b24893afeeefa88594cace26da110c9b751bb
kigawa <mail@kigawa.net>
2025-08-13 23:34:36 +0900

    update: Gradle wrapper to latest version with configuration enhancements

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -14,14 +14,11 @@ allprojects {
 }
 
 nexusPublishing {
+
     // Configure maven central repository
     // https://github.com/gradle-nexus/publish-plugin#publishing-to-maven-central-via-sonatype-ossrh
     repositories {
-        sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            username.set(System.getenv("ORG_GRADLE_PROJECT_sonatypeUsername") ?: project.findProperty("sonatypeUsername") as String?)
-            password.set(System.getenv("ORG_GRADLE_PROJECT_sonatypePassword") ?: project.findProperty("sonatypePassword") as String?)
-        }
+        sonatype()
+
     }
 }
